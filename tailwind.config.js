e142d92725d82e21b80d0aeac5f6d503b003b0d2
Mercedes Lobos <lobos.mercedes.dev@gmail.com>
2024-06-17 17:16:21 -0300

    Add tailwind and theme first version

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      light: '#dcbc97',
+      dark: '#5e3724',
+      primary: '#a7632c',
+      secondary: '#2c70a7',
+      info: '#a02ca7',
+      success: '#549d1d',
+      warning: '#febd52',
+      danger: '#ed1060',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
